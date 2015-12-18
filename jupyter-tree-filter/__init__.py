a43042a89d9feb9632d6cd2bfc20ff4220c3b333
Jonathan Frederic <jon.freder@gmail.com>
2015-12-17 17:01:28 -0800

    Add section

--- jupyter-tree-filter/__init__.py
+++ jupyter-tree-filter/__init__.py
@@ -1,5 +1,6 @@
 def _jupyter_nbextension_paths():
     return [{
+        'section': 'tree',
         'src': 'amd',
         'dest': 'jupyter-tree-filter',
         'require': 'jupyter-tree-filter/index'
